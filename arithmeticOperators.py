7a78bc4ea573b3906c5e2170f420c6db253fa894
Mark Peters <mapeters101@earthlink.net>
2024-10-05 11:09:19 -0400

    Lesson027: Work with arithmetic operators.

---
+++ arithmeticOperators.py
@@ -0,0 +1,28 @@
+print(10 + 20)
+print(10 + 20 + 15)
+print(10 * 20)
+print(3 * 6 * 2)
+print(3 + 6 * 2)
+print((3 + 6) * 2)
+print((3 + 6) * (4 -2))
+a = 10
+b = 20
+print(a + b)
+res_sum = a + b
+print(res_sum)
+res_mul = a * b
+print(res_mul)
+n = 5
+print(n * n + 1 / 2)
+print(n * (n + 1) / 2)
+print((n * (n + 1)) / 2)
+sum_of_integers = (n * (n + 1)) / 2
+print(sum_of_integers)
+a = input('Enter value for a: ')
+b = input('Enter value for b: ')
+res = a + b
+print(res)
+print(type(a))
+print(type(b))
+res = int(a) + int(b)
+print(res)
